34e11f85c7e9aa9fa0fa720eca45541ebe2e6cea
yawwusu <yawwusugh@gmail.com>
2021-06-22 21:29:57 +0200

    Designed and completely styled Header component
    - Responsive

---
+++ components/HeadBanner.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import HeaderOption from "./HeaderOption";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+function HeadBanner() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderOption title="HOME" Icon={HomeIcon} />
+        <HeaderOption title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderOption title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderOption title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderOption title="SEARCH" Icon={SearchIcon} />
+        <HeaderOption title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <div>
+        <Image
+          className="object-contain"
+          src="https://links.papareact.com/ua6"
+          width={200}
+          height={100}
+        />
+      </div>
+    </header>
+  );
+}
+
+export default HeadBanner;
